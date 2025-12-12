019f92e98e4a529766d7c0eeffa2b5c075ca4cf5
Vcore2348 <christianfariello678@gmail.com>
2025-12-11 21:13:51 -0400

    Actualización #2 - (Funcionamiento de la calculadora integrado)

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def calculadora():
-    return render_template("calculadora.html")
+    pantalla = request.form.get("pantalla", "0")
+    boton = request.form.get("boton")
+
+
+    #
+    # Este códgio es para recibir señales del formulario que se encuentra en la calculadora.html
+    #
+    if boton:
+        if boton == "C":
+            pantalla = "0"
+
+        elif boton == "=":
+            try:
+                pantalla = str(eval(pantalla))
+            
+            except:
+                pantalla = "Error"
+
+        else:
+            if pantalla == "0":
+                pantalla = boton
+
+            else:
+                pantalla += boton
+
+    return render_template("calculadora.html", pantalla=pantalla)
